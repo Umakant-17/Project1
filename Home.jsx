375afcd6f7b9235e6d0edc3ac064a42cbe7f454a
Umakant-17 <Kesareuma@gmail.com>
2024-08-08 19:21:23 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,45 @@
+
+const Home=()=>{
+
+return(
+    <>
+   <div className="main" >
+<div className="Header">
+
+<div className="logo">
+    <img src="https://serve2business.com/wp-content/uploads/2021/09/unnamed.png" alt="error" height="80rem"></img>
+</div>
+<div className=" Searchbar">
+    <input type="Search" className="bar" placeholder="search the product">
+
+    </input>
+    <button  className="Searchb1" >Search</button>
+</div>
+<div className="Login">
+    <a href="">Login</a>
+    <i className="fa fa-user"></i>
+</div>
+<div className="Register"> <a href="">Register</a>
+<i className="fa fa-edit"></i>
+</div>
+<div className="Addtocart">
+    <i className="fa fa-shopping-cart"></i>
+
+</div>
+</div>
+<div className="Middle">
+
+</div>
+<div className="Footer">
+
+
+</div>
+
+
+
+   </div>
+    </>
+)
+
+}
+export default Home;
